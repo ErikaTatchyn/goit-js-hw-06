48c08724972688e4894c5021a171049898e15a59
Erika Tatchyn <erika.ucu@gmail.com>
2022-12-16 21:51:24 +0000

    fix

--- js/task-03.js
+++ js/task-03.js
@@ -14,7 +14,6 @@ const images = [
 ];
 
 const galleryListEl = document.querySelector(".gallery");
-galleryListEl.style.display = "flex";
 
 const makeGalleryCard = ({ url, alt } = {}) => {
   return `
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("#validation-input");
 
 const onBlur = () => {
-  if (input.value.length === input.dataLength) {
+  if (input.value.length === 6) {
     input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
